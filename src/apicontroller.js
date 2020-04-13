ccc8720c489057306449f24b437763ce71e9982d
betkibet <kibetnamanga@gmail.com>
2020-04-13 21:36:35 +0300

    Update apicontroller.js

--- src/apicontroller.js
+++ src/apicontroller.js
@@ -52,10 +52,6 @@ app.get('/api/v1/on-covid-19/logs', (req, res) => {
     return res.send(fileContent);
   });
 });
-// const server = app.listen(process.env.PORT || 5000);
-// server.address().port;
-// console.log("Express is working on port " + port);
-const server = app.listen(process.env.PORT || 5000, function () {
-  const port = server.address().port;
-  console.log("Express is working on port " + port);
-});
+const port = process.env.PORT || 5000;
+app.listen(port);
+// console.log('server started on port ' + port);
