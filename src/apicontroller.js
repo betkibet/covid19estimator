bad5c83c85047963d9d0a96f6b979c102a366809
betkibet <kibetnamanga@gmail.com>
2020-04-14 12:33:10 +0300

    Update apicontroller.js

--- src/apicontroller.js
+++ src/apicontroller.js
@@ -13,16 +13,17 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // create logger here
-morgan.token('response-time', (req, res) => {
-  const start = new Date().getMilliseconds();
-  const end = new Date().getMilliseconds() * 0.5;
-  let time = start - end;
-  time = Math.round(time);
-  time = time.toString().padStart(2, 0);
-  time = +time;
-  return time + 'ms';
-});
-const loggerFormat = ':method :url :status :response-time';
+// morgan.token('response-time', (req, res) => {
+//   req.headers['content-type'];
+//   const start = new Date().getMilliseconds();
+//   const end = new Date().getMilliseconds() * 0.5;
+//   let time = start - end;
+//   time = Math.round(time);
+//   time = time.toString().padStart(2, 0);
+//   time = +time;
+//   return time;
+// });
+const loggerFormat = ':method\t\t:url\t\t:status\t\t:response-time[0]ms';
 app.use(morgan(loggerFormat, {
   stream: fs.createWriteStream('./access.log', { flags: 'a' })
 }));
